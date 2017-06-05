9c1cf3781763c315322cdb5c6fa357e51d3c1a56
mtelesz <michaltelesz@users.noreply.github.com>
2017-06-05 20:37:22 +0200

    Update requests views

--- Helpdesk.WebUI/App_Start/RouteConfig.cs
+++ Helpdesk.WebUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Helpdesk.WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Statuses", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Requests", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
